e9a08e86800de5b4d4d6b4ad0f358899c9dc7764
Filip Sivák <sivakfil@gmail.com>
2020-04-11 11:37:02 +0200

    Page height bugfix.

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,25 @@ import { DotEffect } from './DotEffect';
 
 window.addEventListener("DOMContentLoaded", () => {
     const canvasElement = document.querySelector("canvas");
-    canvasElement.width = document.documentElement.clientWidth;
-    canvasElement.height = document.documentElement.clientHeight;
-    
-    console.log(canvasElement.width, canvasElement.height);
+    canvasElement.width = document.body.clientWidth;
+    canvasElement.height = document.body.clientHeight;
+
+    console.log(document.body.clientWidth, document.body.clientHeight);
 
     const engine = new GameEngine(canvasElement);
     const dotEffect = new DotEffect(engine, 200);
     dotEffect.run();
 
     window.addEventListener("resize", (event) => {
-        console.log(event);
-        canvasElement.width = document.documentElement.clientWidth;
-        canvasElement.height = document.documentElement.clientHeight;
+        canvasElement.width = document.body.clientWidth;
+        canvasElement.height = document.body.clientHeight;
     });
+
+    // bugfix
+    window.setTimeout(() => {
+        canvasElement.width = document.body.clientWidth;
+        canvasElement.height = document.body.clientHeight;
+    }, 500);
 });
+
+console.log("%cLike the code? Consider hiring me at sivakfil@gmail.com!", "background: red; color: yellow; font-size: x-large");
